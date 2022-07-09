bcd3b66f166d423a0cec2c70c32483193231888c
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-07-09 16:57:05 -0700

    dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl:
+    'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '80218d2ed4msh6ce325a1a35ed79p195a8djsn5e7d16b74461',
 };
